1356084d85b78bca4402c6101ff3ec9194a400c2
Ryoichiro Kitahara <175901292+ryoichi1470@users.noreply.github.com>
2024-11-14 10:42:04 +0000

    add shelter controller

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,7 +1,10 @@
 class SheltersController < ApplicationController
   def index
+    @shelters = Shelter.all
+    gon.shelters = @shelters
   end
 
   def show
+    @shelter = Shelter.find(params[:id])
   end
 end
